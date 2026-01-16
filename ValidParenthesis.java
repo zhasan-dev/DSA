c7f0b6e569cb18432199d923dd660434b2d12956
zhasan-dev <your-new-github-email@example.com>
2026-01-16 11:49:10 +0530

    version 3

---
+++ ValidParenthesis.java
@@ -0,0 +1,14 @@
+public class ValidParenthesis {
+    public boolean isValid(String s){
+        java.util.Stack<Character> stack = new java.util.Stack<>();
+        for(char c : s.toCharArray()){
+            if(c == '(') stack.push(')');
+            else if(c == '{') stack.push('}');
+            else if(c == '[') stack.push(']');
+            else{
+                if(stack.isEmpty() || stack.pop() != c) return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
