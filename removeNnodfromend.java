b038b283c60d4b609a6ac6e5dfc461e6d6dba18b
zhasan-dev <zhasan.dev@gmail.com>
2026-01-19 11:36:44 +0530

    4th day

---
+++ removeNnodfromend.java
@@ -0,0 +1,25 @@
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode fast = dummy;
+        ListNode slow = dummy;
+
+        // Move fast n+1 steps ahead
+        for (int i = 0; i <= n; i++) {
+            fast = fast.next;
+        }
+
+        // Move both until fast hits null
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        // Delete node
+        slow.next = slow.next.next;
+
+        return dummy.next;
+    }
+}
