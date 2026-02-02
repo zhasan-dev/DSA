c811619167e1d7c99df0f082ada07f7b75aa0a38
zhasan-dev <zhasan.dev@gmail.com>
2026-02-02 23:46:02 +0530

    Reorganised files and did water cells question

---
+++ CodeForces/Main.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0){
+            int n =sc.nextInt();
+            String s = sc.next();
+            int water = 0;
+            int count =0;
+            for(int i=0; i<n; i++){
+                if(s.charAt(i)=='.'){
+                    water++;
+                    count++;
+                    if(count==3){
+                        water=2;
+                        break;
+                    }
+                }
+                else{
+                    count=0;
+                }
+            }
+
+            System.out.println(water);
+        }
+    }
+}
